c9107fe581e7d9e10010183478043ca5320c0517
Tony Air <tony@twma.pro>
2022-02-09 23:09:19 +0200

    FIX: mysql dump gzip is on

--- src/Tasks/DumpMySQL.php
+++ src/Tasks/DumpMySQL.php
@@ -14,6 +14,7 @@ class DumpMySQL extends BuildTask
 
     public function run($request)
     {
+        $fileName = 'backup-'.date('d-m-Y').'.sql';
         $cfg = DB::getConfig();
 
         try {
@@ -21,7 +22,15 @@ class DumpMySQL extends BuildTask
         } catch (Exception $e) {
         }
 
-        header('Content-Disposition: attachment; filename="backup-'.date('d-m-Y').'.sql"');
+        // check if gzip is on
+        try {
+            if (count(array_intersect(['mod_deflate', 'mod_gzip'], apache_get_modules())) > 0) {
+                $fileName .= '.gz';
+            }
+        } catch (Exception $e) {
+        }
+
+        header('Content-Disposition: attachment; filename="'.$fileName.'"');
         passthru('mysqldump -u '.$cfg['username'].' --password="'.$cfg['password'].'" '.$cfg['database']);
         exit(0);
     }
