39f81b5a163dd036138c4695247d240a4b22201f
Tony Air <tony@twma.pro>
2023-10-24 22:00:52 +0200

    IMPR: GraphQL helpers

---
+++ src/Extensions/ObjectGraphQlEx.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace A2nt\CMSNiceties\Extensions;
+
+use SilverStripe\Control\Controller;
+use SilverStripe\GraphQL\Controller as GraphQLController;
+use App\GraphQL\URLLinkablePlugin;
+use SilverStripe\ORM\DataExtension;
+
+/**
+ * Class \A2nt\CMSNiceties\Extensions\SubmittedFormEx
+ *  AJAX/GraphQL helpers
+ * @property \A2nt\CMSNiceties\Extensions\SubmittedFormEx $owner
+ */
+class ObjectGraphQlEx extends DataExtension
+{
+    // Get rendered template
+    public function MainContent()
+    {
+        $object = $this;
+        return isset($object->GraphQLContent) ? $object->GraphQLContent : null;
+    }
+
+    public function RequestLink()
+    {
+        $curr = Controller::curr();
+        //$var = URLLinkablePlugin::config()->get('single_field_name');
+        $var = 'url';
+        if ($curr::class === GraphQLController::class) {
+            $vars = json_decode($curr->getRequest()->getBody(), true)['variables'];
+            if (isset($vars[$var])) {
+                return $vars[$var];
+            }
+        }
+
+        return null;
+    }
+}
